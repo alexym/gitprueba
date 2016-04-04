3a1865607764afb7079a7a2317359a8432166e9b
alexym <alexym111@gmail.com>
2016-04-04 12:43:44 -0500

    Initial Commit

---
+++ pruebagit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pruebagit
+//
+//  Created by Cloud Co on 04/04/16.
+//  Copyright © 2016 Cloud Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
